2af785c5412ce37da6d2f026dbc972936c2f39cc
Just_Ho_it <zalop3128@kakao.com>
2024-04-07 19:54:00 +0900

    Initial commit

---
+++ src/main/java/com/example/springoauth2clientsession/SpringOauth2ClientSessionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springoauth2clientsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2ClientSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauth2ClientSessionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springoauth2clientsession/SpringOauth2ClientSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springoauth2clientsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ClientSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
